836dc58005f0c037efedc6b273074ab0425bff90
Sadab Khan <Sadab Khan@DESKTOP-M2I3102>
2020-12-19 18:13:29 +0530

    1st file update

--- src/com/sa/array/ArrayInsert.java
+++ src/com/sa/array/ArrayInsert.java
@@ -1,10 +1,13 @@
 package com.sa.array;
 
+import java.util.Scanner;
+
 public class ArrayInsert {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sc =new Scanner(System.in);
+	System.out.println("Enter the Array size");
+	int arr=sc.nextInt();
 	}
 
 }
